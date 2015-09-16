32827891998f9f8c9e11294e91c4f67340989218
Jordan Roth <jmtroth0@gmail.com>
2015-09-16 10:29:46 -0400

    Add dropdowns and non-functioning button to navbar

--- app/assets/javascripts/views/navbar.js
+++ app/assets/javascripts/views/navbar.js
@@ -3,10 +3,13 @@ Sparklr.Views.NavBar = Backbone.View.extend({
   tagName: 'nav',
   className: 'main-navbar',
 
-  events: {},
 
   render: function () {
     this.$el.html(this.template());
     return this;
   },
+
+  signOut: function () {
+    
+  }
 })
