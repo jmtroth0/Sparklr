c7c18522463c7bac72730629fbbab9a316bdc0f0
Jordan Roth <jmtroth0@gmail.com>
2015-09-27 22:41:30 -0400

    Fix includes bug

--- app/controllers/api/photostreams_controller.rb
+++ app/controllers/api/photostreams_controller.rb
@@ -1,11 +1,11 @@
 module Api
   class PhotostreamsController < ApplicationController
     def show
-      @photostream = current_user.photostream.includes(:photos)
+      @photostream = current_user.photostream
     end
 
     def user_show
-      @photostream = User.find(params[:user_id]).photostream.includes(:photos)
+      @photostream = User.find(params[:user_id]).photostream
       render :show
     end
   end
